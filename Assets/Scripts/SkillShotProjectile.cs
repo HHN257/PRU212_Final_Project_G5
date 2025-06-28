a7527b2a593676f1de6bf4be5e31cfa3ca6fd52c
HHN257 <hoanghuunam257@gmail.com>
2025-06-28 21:59:31 +0700

    Add player skillshot and fix player animation

---
+++ Assets/Scripts/SkillShotProjectile.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class SkillShotProjectile : MonoBehaviour
+{
+    public int damage = 1;
+    public float lifeTime = 3f;
+
+    private void Start()
+    {
+        Destroy(gameObject, lifeTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            EnemyHealth enemy = other.GetComponent<EnemyHealth>();
+            if (enemy != null)
+            {
+                enemy.TakeDamage(damage, transform.position);
+            }
+            Destroy(gameObject);
+        }
+        else if (other.CompareTag("Boss"))
+        {
+            BossController boss = other.GetComponent<BossController>();
+            if (boss != null)
+            {
+                boss.TakeDamage(damage); // BossController.TakeDamage(float amount)
+            }
+            Destroy(gameObject);
+        }
+        else if (other.CompareTag("Ground"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
